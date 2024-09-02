4482c650bf70dbfefe576da6546b7dda8e13a77c
Stephendass <stephen.stainluss@gmail.com>
2024-09-02 12:10:02 +0530

    Writing code for 1D array

---
+++ 1D Array/array.java
@@ -0,0 +1,29 @@
+//import java.util.Arrays;
+import java.util.Scanner;
+
+public class array{
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        
+        //take input in array from user
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        // print the value of array
+        int totalScore = 0;
+        for(int i = 0; i < n; i++){
+            totalScore += arr[i];
+        }
+        
+        double avg = totalScore/(double)n;
+        System.out.println(avg);
+
+        sc.close();
+    }
+}
+
+
