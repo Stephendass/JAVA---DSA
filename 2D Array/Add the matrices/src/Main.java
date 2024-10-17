cead48e4e5eeb918ff7bbe56338ad082561676dc
Stephendass <stephen.stainluss@gmail.com>
2024-10-17 16:59:05 +0530

    Ex about adding two matrices

--- 2D Array/Add the matrices/src/Main.java
+++ 2D Array/Add the matrices/src/Main.java
@@ -1,6 +1,23 @@
 import java.sql.SQLOutput;
 import java.util.*;
+/*
+* You are given two matrices A & B of same size,
+* you have to return another matrix which is the sum of A and B.
+*
+* Example Input 1:
+A = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
 
+B = [[9, 8, 7],
+     [6, 5, 4],
+     [3, 2, 1]]
+*
+* Example Output
+[[10, 10, 10],
+ [10, 10, 10],
+ [10, 10, 10]]
+* */
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
